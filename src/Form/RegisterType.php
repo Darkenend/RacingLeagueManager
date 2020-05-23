7c09f0e74fc6c657ab12ca26b063dcae6c148615
Álvaro 'Darkenend' Real <34766606+Darkenend@users.noreply.github.com>
2020-05-23 20:13:58 +0200

    Registration System
    Signed-off-by: Álvaro 'Darkenend' Real <34766606+Darkenend@users.noreply.github.com>

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'maxlength' => '64'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'maxlength' => '128'
+                ]
+            ])
+            ->add('steamid', TextType::class, [
+                'label' => 'Your SteamID64',
+                'attr' => [
+                    'maxlength' => '18'
+                ]
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

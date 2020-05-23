9ab30383d85f933122ee0122d650a93e6201a653
Álvaro 'Darkenend' Real <34766606+Darkenend@users.noreply.github.com>
2020-05-23 18:44:19 +0200

    Message Creation Hotfix
    Signed-off-by: Álvaro 'Darkenend' Real <34766606+Darkenend@users.noreply.github.com>

---
+++ src/Form/ConversationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conversation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConversationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user', EntityType::class, [
+                'choice_label' => '__toString'
+            ])
+            ->add('topic', TextType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Conversation::class,
+        ]);
+    }
+}
